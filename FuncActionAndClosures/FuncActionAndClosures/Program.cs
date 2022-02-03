332362afc31c805513f7de9b1065956db625b1da
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-03 23:41:10 +0600

    Func, Action & Closures - 2(Funcs)

--- FuncActionAndClosures/FuncActionAndClosures/Program.cs
+++ FuncActionAndClosures/FuncActionAndClosures/Program.cs
@@ -60,6 +60,58 @@ MeasureTimeTakingActionAsParam2((int n) =>
 
     Console.WriteLine($"Inside Anonymous delegate that is used as Action, value of n after calculation: {n}");
 });
+/*
+ * So Actions are Pre-Defined Delegates that takes in parameters but return type is void
+ */
 
 
 
+
+
+
+
+void MeasureTimeTakingFuncAsParam(Func<int> func)
+{
+    var stopwatch = Stopwatch.StartNew();
+    var result = func();
+    result -= 1000;
+    Console.WriteLine($"Inside MeasureTimeTakingFuncAsParam, value of the result is: {result}");
+}
+
+int FuncFunction()
+{
+    var n = 2;
+    for (int i = 0; i < 100000000; i++)
+    {
+        n += 1;
+    }
+
+    return n;
+}
+
+MeasureTimeTakingFuncAsParam(FuncFunction);
+
+
+
+void MeasureTimeTakingFuncAsParam2(Func<int, int> func)
+{
+    var stopwatch = Stopwatch.StartNew();
+    var result = func(3);
+    result -= 1000;
+    Console.WriteLine($"Inside MeasureTimeTakingFuncAsParam2, value of the result is: {result}");
+}
+
+MeasureTimeTakingFuncAsParam2(int (n) =>
+{
+    for (int i = 0; i < 100000000; i++)
+    {
+        n += 1;
+    }
+    
+    Console.WriteLine($"Inside the Func of MeasureTimeTakingFuncAsParam2, value of the n is: {n}");
+
+    return n;
+});
+/*
+ * So Funcs are Pre-Defined Delegates that takes in parameters and has a return type
+ */
