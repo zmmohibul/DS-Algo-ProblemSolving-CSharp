3328502ab41034aa9bb1a2408b3a125de052f473
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-09 00:32:47 +0600

    Frequency Counter Pattern - Valid Anagram

--- problem-solving-pattern/FrequencyCounterPattern/FrequencyCounterPattern/Program.cs
+++ problem-solving-pattern/FrequencyCounterPattern/FrequencyCounterPattern/Program.cs
@@ -1,4 +1,4 @@
-﻿bool same(List<int> list1, List<int> list2)
+﻿bool Same(List<int> list1, List<int> list2)
 {
     if (list1.Count != list2.Count)
     {
@@ -30,10 +30,10 @@
     }
 
     Console.WriteLine("freqList1: ");
-    printDictionary(freqList1);
+    PrintDictionary(freqList1);
 
     Console.WriteLine("freqList2: ");
-    printDictionary(freqList2);
+    PrintDictionary(freqList2);
 
     foreach (var (key, value) in freqList1)
     {
@@ -41,19 +41,70 @@
         {
             return false;
         }
-        else
+        
+        if (freqList2[key * key] != freqList1[key])
         {
-            if (freqList2[key * key] != freqList1[key])
-            {
-                return false;
-            }
+            return false;
         }
     }
 
     return true;
 }
 
-void printDictionary(IDictionary<int, int> dictionary)
+bool ValidAnagram(string s1, string s2)
+{
+    if (s1.Length != s2.Length)
+    {
+        return false;
+    }
+    
+    var s1CharFreq = new Dictionary<char, int>();
+    var s2CharFreq = new Dictionary<char, int>();
+
+    for (int i = 0; i < s1.Length; i++)
+    {
+        if (!s1CharFreq.ContainsKey(s1[i]))
+        {
+            s1CharFreq[s1[i]] = 1;
+        }
+        else
+        {
+            s1CharFreq[s1[i]] += 1;
+        }
+        
+        if (!s2CharFreq.ContainsKey(s2[i]))
+        {
+            s2CharFreq[s2[i]] = 1;
+        }
+        else
+        {
+            s2CharFreq[s2[i]] += 1;
+        }
+    }
+
+    Console.WriteLine("s1CharFreq:");
+    PrintDictionary(s1CharFreq);
+    
+    Console.WriteLine("s2CharFreq:");
+    PrintDictionary(s2CharFreq);
+
+    foreach (var (key, value) in s1CharFreq)
+    {
+        if (!s2CharFreq.ContainsKey(key))
+        {
+            return false;
+        }
+
+        if (s2CharFreq[key] != value)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+void PrintDictionary<T>(IDictionary<T, int> dictionary)
 {
     foreach (var (key, value) in dictionary)
     {
@@ -61,4 +112,5 @@ void printDictionary(IDictionary<int, int> dictionary)
     }
 }
 
-Console.WriteLine(same(new List<int>() {1, 2, 2, 2, 3, 3}, new List<int>() {9, 1, 4, 4, 8, 9}));
+Console.WriteLine(Same(new List<int>() {1, 2, 2, 2, 3, 3}, new List<int>() {9, 1, 4, 4, 8, 9}));
+Console.WriteLine(ValidAnagram("ababcaa", "caabbaw"));
