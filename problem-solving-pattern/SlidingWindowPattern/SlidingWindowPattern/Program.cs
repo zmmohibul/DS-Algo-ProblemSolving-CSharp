c24efc5f9962630e990676721cdd65f9bd16ec0a
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-10 01:57:35 +0600

    Sliding Window Pattern - MaxSubArraySum

--- problem-solving-pattern/SlidingWindowPattern/SlidingWindowPattern/Program.cs
+++ problem-solving-pattern/SlidingWindowPattern/SlidingWindowPattern/Program.cs
@@ -1,10 +1,33 @@
 ﻿int MaxSubArraySum(List<int> list, int n)
 {
-    var maxSum = int.MinValue;
     
-    
+    /*
+     * [1, 4, 6, 9, 13, 17, 22, 29, 31, 83, 88, 91, 94, 97]
+     * 
+     */
 
+    var maxSum = 0;
+    var tempSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        tempSum += list[i];
+        maxSum += list[i];
+    }
+
+    var j = 0;
+    for (int i = n; i < list.Count; i++)
+    {
+        tempSum += list[i];
+        tempSum -= list[j];
+        if (tempSum > maxSum)
+        {
+            maxSum = tempSum;
+        }
+        j += 1;
+    }
+    
     return maxSum;
 }
 
-Console.WriteLine("Hello, World!");
+var l = new List<int>() {1, 4, 6, 9, 13, 88, 91, 94, 95, 17, 22, 29, 31, 83, 88, 91, 94, 96};
+Console.WriteLine(MaxSubArraySum(l, 4));
