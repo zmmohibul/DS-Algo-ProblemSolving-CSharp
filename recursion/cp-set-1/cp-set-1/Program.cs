1506b2da809b9445383afa5d8e7228d083968b9a
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-10 20:08:18 +0600

    Recursion - ProductOfList

--- recursion/cp-set-1/cp-set-1/Program.cs
+++ recursion/cp-set-1/cp-set-1/Program.cs
@@ -1,23 +1,50 @@
-﻿int Power(int baseNum, int exponent)
+﻿
+
+Console.WriteLine(Recursion.Power(2, 4));
+Console.WriteLine(Recursion.Factorial(7));
+
+var l = new List<int>() {1, 2, 3, 4, 5};
+// Console.WriteLine(l[^1]);
+// Console.WriteLine(l);
+// l.RemoveAt(l.Count - 1);
+// l.ForEach(Console.Write);
+
+Console.WriteLine(Recursion.ProductOfList(l));
+
+class Recursion
 {
-    if (exponent == 0)
+    public static int ProductOfList(List<int> numbers)
     {
-        return 1;
+        if (numbers.Count == 0)
+        {
+            return 1;
+        }
+
+        var lastValue = numbers[^1];
+        numbers.RemoveAt(numbers.Count - 1);
+        
+        return lastValue * ProductOfList(numbers);
+    }
+    
+    public static int Power(int baseNum, int exponent)
+    {
+        if (exponent == 0)
+        {
+            return 1;
+        }
+
+        return baseNum * Power(baseNum, exponent - 1);
     }
 
-    return baseNum * Power(baseNum, exponent - 1);
-}
-
-int Factorial(int n)
-{
-    if (n == 0)
+    public static int Factorial(int n)
     {
-        return 1;
-    }
+        if (n == 0)
+        {
+            return 1;
+        }
 
-    return n * Factorial(n - 1);
+        return n * Factorial(n - 1);
+    }
 }
 
 
-Console.WriteLine(Power(2, 4));
-Console.WriteLine(Factorial(7));
