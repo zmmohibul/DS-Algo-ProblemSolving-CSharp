40dcd82831d4cd4a410ad6be44fd44d62002e726
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-10 21:26:51 +0600

    Recursion - SumOfArray

--- recursion/cp-set-1/cp-set-1/Program.cs
+++ recursion/cp-set-1/cp-set-1/Program.cs
@@ -9,6 +9,7 @@ var l = new List<int>() {1, 2, 3, 4, 5};
 // l.RemoveAt(l.Count - 1);
 // l.ForEach(Console.Write);
 
+Console.WriteLine(Recursion.SumOfArray(l));
 Console.WriteLine(Recursion.ProductOfList(l));
 
 class Recursion
@@ -25,7 +26,21 @@ class Recursion
         
         return lastValue * ProductOfList(numbers);
     }
-    
+
+    public static int SumOfArray(List<int> numbers)
+    {
+        return SumOfArray(numbers, 0);
+    }
+
+    public static int SumOfArray(List<int> numbers, int i)
+    {
+        if (i == numbers.Count)
+        {
+            return 0;
+        }
+
+        return numbers[i] + SumOfArray(numbers, i += 1);
+    }
     public static int Power(int baseNum, int exponent)
     {
         if (exponent == 0)
