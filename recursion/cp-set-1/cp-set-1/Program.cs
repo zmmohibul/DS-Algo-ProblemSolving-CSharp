3c8e49467a07a18e2ba17a45c1f21bc2edb68039
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-10 19:50:17 +0600

    Recursion - Factorial

--- recursion/cp-set-1/cp-set-1/Program.cs
+++ recursion/cp-set-1/cp-set-1/Program.cs
@@ -8,4 +8,16 @@
     return baseNum * Power(baseNum, exponent - 1);
 }
 
+int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    return n * Factorial(n - 1);
+}
+
+
 Console.WriteLine(Power(2, 4));
+Console.WriteLine(Factorial(7));
