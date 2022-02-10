714f85e6dba4cad7172c7ba3179fcf1578221128
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-10 19:42:20 +0600

    Recursion - Power

---
+++ recursion/cp-set-1/cp-set-1/Program.cs
@@ -0,0 +1,11 @@
+﻿int Power(int baseNum, int exponent)
+{
+    if (exponent == 0)
+    {
+        return 1;
+    }
+
+    return baseNum * Power(baseNum, exponent - 1);
+}
+
+Console.WriteLine(Power(2, 4));
