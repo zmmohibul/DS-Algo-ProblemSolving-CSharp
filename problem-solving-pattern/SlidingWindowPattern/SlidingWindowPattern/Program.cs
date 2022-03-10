b8688be4645b2c06b8ed181fbe24fa6826356bd3
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-10 13:58:12 +0600

    Sliding Window Pattern - LongestSubstring - 1

--- problem-solving-pattern/SlidingWindowPattern/SlidingWindowPattern/Program.cs
+++ problem-solving-pattern/SlidingWindowPattern/SlidingWindowPattern/Program.cs
@@ -1,4 +1,6 @@
-﻿int MaxSubArraySum(List<int> list, int n)
+﻿using System.Text;
+
+int MaxSubArraySum(List<int> list, int n)
 {
     
     /*
@@ -29,5 +31,35 @@
     return maxSum;
 }
 
+string FindLongestSubstring(string s) 
+{
+    //  "longestsubstring"
+    //   0123456789012345 
+    StringBuilder sb = new StringBuilder();
+    int maxLength = 0;
+    Dictionary<char, bool> containsChar = new Dictionary<char, bool>();
+    int i, j;
+    for (i = 0, j = 1; j < s.Length; j++) {
+        containsChar[s[i]] = true;
+        if (containsChar.ContainsKey(s[j])) {
+            containsChar = new Dictionary<char, bool>();
+            i = j;
+            continue;
+        } 
+        else {
+            containsChar[s[j]] = true;
+        }
+    }
+
+    sb.Append(s.Substring(i, j - i));
+    Console.WriteLine(j - i);
+    foreach (var kv in containsChar) {
+        Console.WriteLine(kv.Key);
+    }
+
+    return sb.ToString();
+}
+
 var l = new List<int>() {1, 4, 6, 9, 13, 88, 91, 94, 95, 17, 22, 29, 31, 83, 88, 91, 94, 96};
 Console.WriteLine(MaxSubArraySum(l, 4));
+Console.WriteLine(FindLongestSubstring("longestsubstring"));
