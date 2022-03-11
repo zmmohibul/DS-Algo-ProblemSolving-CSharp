8100c85b8eaa44c74c482d44de76365f188b5226
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-11 15:30:19 +0600

    Sliding Window Pattern - LongestSubstring - 3

--- problem-solving-pattern/SlidingWindowPattern/SlidingWindowPattern/Program.cs
+++ problem-solving-pattern/SlidingWindowPattern/SlidingWindowPattern/Program.cs
@@ -31,41 +31,32 @@ int MaxSubArraySum(List<int> list, int n)
     return maxSum;
 }
 
-string FindLongestSubstring(string s) 
+int FindLongestSubstring(string s) 
 {
     //  "longestsubstring"
     //   0123456789012345 
-    StringBuilder sb = new StringBuilder();
-    int maxLength = 0;
     Dictionary<char, int> containsChar = new Dictionary<char, int>();
-    Dictionary<string, int> longestWords = new Dictionary<string, int>();
 
+    int maxLength = 0;
+    int currentStringLength = 1;
     int i, j;
     for (i = 0, j = 1; j < s.Length; j++) {
         containsChar[s[i]] = i;
-        if (containsChar.ContainsKey(s[j])) {
-            sb.Insert(0, s[i]);
-            i = containsChar[s[j]] + 1;
+        if (!containsChar.ContainsKey(s[j])) {
+            containsChar[s[j]] = j;
+            currentStringLength += 1;
+        } else {
+            if (currentStringLength > maxLength) {
+                maxLength = currentStringLength;
+            }
+            i = j - 1;
             containsChar = new Dictionary<char, int>();
             containsChar[s[j]] = j;
-            containsChar[s[i]] = i;
-            longestWords[sb.ToString()] = sb.Length;
-            sb = new StringBuilder();
-            sb.Append(s[j]);
-        } 
-        else {
-            sb.Append(s[j]);
-            containsChar[s[j]] = j;
+            currentStringLength = 2;
         }
     }
 
-    sb.Append(s.Substring(i, j - i));
-    Console.WriteLine(j - i);
-    foreach (var kv in containsChar) {
-        Console.WriteLine(kv.Key);
-    }
-
-    return sb.ToString();
+    return maxLength;
 }
 
 var l = new List<int>() {1, 4, 6, 9, 13, 88, 91, 94, 95, 17, 22, 29, 31, 83, 88, 91, 94, 96};
