0859c660515c62f6d4f8bb40e5c276f908c8c58f
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-22 22:08:04 +0600

    PingPongChallenge - works

--- EverForthCodingTest/Program.cs
+++ EverForthCodingTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.Collections;
 void AvailableTime(List<List<int>> arr) 
 {
     // var availableTimeOfPlayers = new List<Dictionary<int, bool>>();
@@ -70,38 +72,166 @@ void AvailableTime(List<List<int>> arr)
 
     // System.Console.WriteLine(playerAvailabilityTimeTable[0][0]);
 
-    var playerAvailabilityTimeTable = new List<List<Tuple<int, int>>>();
+    // var playerAvailabilityTimeTable = new List<List<Tuple<int, int>>>();
 
-    foreach (var item in arr) 
+    // foreach (var item in arr) 
+    // {
+    //     var playerStartTime = item[0];
+    //     var playerEndTime = item[1];
+
+    //     var playerTimeAvailability = new List<Tuple<int, int>>();
+    //     for (int i = playerStartTime; i < playerEndTime; i++)
+    //     {
+    //         playerTimeAvailability.Add(new Tuple<int, int>(i, i + 1));
+    //     }
+    //     playerAvailabilityTimeTable.Add(playerTimeAvailability);
+    // }
+
+    // var pi = 1;
+    // foreach (var item in playerAvailabilityTimeTable)
+    // {
+    //     System.Console.Write($"Player-{pi}: ");
+    //     foreach (var element in item) 
+    //     {
+    //         System.Console.Write(element.ToString() + ", ");
+    //     }
+    //     System.Console.WriteLine();
+    //     pi += 1;
+    // }
+
+    // for (int i = 0; i < playerAvailabilityTimeTable.Count; i++)
+    // {
+
+    // }
+
+    // var playerAvailabilityTimeTable = new List<Dictionary<Tuple<int, int>, bool>>();
+
+    // foreach (var item in arr) 
+    // {
+    //     var startTime = item[0];
+    //     var endTime = item[1];
+
+    //     var playerTimeAvailability = new Dictionary<Tuple<int, int>, bool>();
+    //     for (int i = startTime; i < endTime; i++)
+    //     {
+    //         playerTimeAvailability[new Tuple<int, int>(i, i + 1)] = true;
+    //     }
+    //     playerAvailabilityTimeTable.Add(playerTimeAvailability);
+    // }
+
+    // foreach (var item in playerAvailabilityTimeTable)
+    // {
+    //     foreach (var (key, value) in item) 
+    //     {
+    //         System.Console.Write($"{key.ToString()}, ");
+    //     }
+    //     System.Console.WriteLine();
+    // }
+
+
+    // for (int i = 0; i < playerAvailabilityTimeTable.Count; i++) 
+    // {
+    //     var iPlayerTime = playerAvailabilityTimeTable[i];
+
+    //     var playersAvailableAtSlot = new Dictionary<Tuple<int, int>, int>();
+
+    //     foreach (var (timeSlot, available) in iPlayerTime) 
+    //     {
+            
+    //         for (int j = i + 1; j < playerAvailabilityTimeTable.Count; j++) 
+    //         {
+    //             if (playerAvailabilityTimeTable[j].ContainsKey(timeSlot) && playerAvailabilityTimeTable[j][timeSlot])
+    //             {
+                    
+    //             }
+    //         }
+    //     }
+    // }
+
+
+    var timeSlots = new Dictionary<Tuple<int, int>, List<int>>();
+    var playerNumber = 1;
+    foreach(var item in arr) 
     {
-        var playerStartTime = item[0];
-        var playerEndTime = item[1];
+        var startTime = item[0];
+        var endTime = item[1];
 
-        var playerTimeAvailability = new List<Tuple<int, int>>();
-        for (int i = playerStartTime; i < playerEndTime; i++)
+
+        for (int i = startTime; i < endTime; i++) 
         {
-            playerTimeAvailability.Add(new Tuple<int, int>(i, i + 1));
+            var timeSlot = new Tuple<int, int> (i, i + 1);
+            if (!timeSlots.ContainsKey(timeSlot))
+            {
+                timeSlots[timeSlot] = new List<int>();
+                timeSlots[timeSlot].Add(playerNumber);
+            }
+            else
+            {
+                timeSlots[timeSlot].Add(playerNumber);
+            }
+
         }
-        playerAvailabilityTimeTable.Add(playerTimeAvailability);
+
+        playerNumber += 1;
     }
 
-    var pi = 1;
-    foreach (var item in playerAvailabilityTimeTable)
+    // foreach (var (slot, players) in timeSlots)
+    // {
+    //     System.Console.Write(slot.ToString() + ": ");
+    //     foreach (var player in players)
+    //     {
+    //         System.Console.Write(player + ", ");
+    //     }
+
+    //     System.Console.WriteLine();
+    // }
+
+    var doublePlayerMatches = 0;
+    foreach (var (slot, player) in timeSlots)
     {
-        System.Console.Write($"Player-{pi}: ");
-        foreach (var element in item) 
+        if (player.Count >= 4)
         {
-            System.Console.Write(element.ToString() + ", ");
+            doublePlayerMatches += 1;
+            player.RemoveRange(0, 4);
         }
+    }
+
+    System.Console.WriteLine("Number of double matches: " + doublePlayerMatches);
+    foreach (var (slot, players) in timeSlots)
+    {
+        System.Console.Write(slot.ToString() + ": ");
+        foreach (var player in players)
+        {
+            System.Console.Write(player + ", ");
+        }
+
         System.Console.WriteLine();
-        pi += 1;
     }
 
-    for (int i = 0; i < playerAvailabilityTimeTable.Count; i++)
+    var singlePlayerMatches = 0;
+    foreach (var (slot, player) in timeSlots)
     {
-
+        if (player.Count >= 2)
+        {
+            singlePlayerMatches += 1;
+            player.RemoveRange(0, 2);
+        }
     }
+    System.Console.WriteLine("Number of single matches: " + singlePlayerMatches);
+    foreach (var (slot, players) in timeSlots)
+    {
+        System.Console.Write(slot.ToString() + ": ");
+        foreach (var player in players)
+        {
+            System.Console.Write(player + ", ");
+        }
+
+        System.Console.WriteLine();
+    }
+
+    System.Console.WriteLine("hello");
 }
+
 
 // void PrintDictionary(Dictionary<int, bool> d)
 // {
@@ -148,4 +278,11 @@ d[new Tuple<int, int>(10, 11)] = true;
 d[new Tuple<int, int>(11, 12)] = true;
 d[new Tuple<int, int>(13, 14)] = true;
 d.ContainsKey(new Tuple<int, int>(11, 12));
+
+var l1 = new List<int> {1, 2, 3, 4, 5, 6, 7};
+var l2 = new List<int> {1, 2, 3, 4, 5, 6, 7};
+
+l1.RemoveRange(0, 4);
+l1.ForEach(System.Console.WriteLine);
+
 
