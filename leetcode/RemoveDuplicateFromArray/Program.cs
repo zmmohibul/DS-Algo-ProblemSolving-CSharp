3839c082c3afbe7171ddf3830da140f20cf85f01
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-28 17:13:20 +0600

    LeetCode - Remove Duplicate From Array

---
+++ leetcode/RemoveDuplicateFromArray/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+System.Console.WriteLine(RemoveDuplicates(new int[]{1, 1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 6}));
+
+
+int RemoveDuplicates(int[] nums) {
+
+    int i = 0;
+    int j = 0;
+
+    while (j < nums.Length) {
+        if (nums[i] == nums[j]) {
+            j += 1;
+        } else {
+            nums[i + 1] = nums[j];
+            i += 1;
+        }
+    }
+        
+    return i + 1;
+}
