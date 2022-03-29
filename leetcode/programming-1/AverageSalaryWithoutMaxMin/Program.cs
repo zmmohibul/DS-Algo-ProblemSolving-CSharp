2563f341beb1dfb03b05c82b06144510e78048fb
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-30 04:12:32 +0600

    P-1 - Average Salary Without Max Min

---
+++ leetcode/programming-1/AverageSalaryWithoutMaxMin/Program.cs
@@ -0,0 +1,30 @@
+﻿double Average(int[] salary) {
+    double average = 0;
+    int maxSalary = int.MinValue;
+    int minSalary = int.MaxValue;
+
+    var salaryLength = salary.Length;
+    if (salaryLength <= 2) {
+        return 0;
+    }
+
+    for (int i = 0; i < salaryLength; i++) {
+        var s = salary[i];
+        average += s;
+
+        if (s > maxSalary) {
+            maxSalary = s;
+        }
+
+        if (s < minSalary) {
+            minSalary = s;
+        }
+    }
+
+    average -= maxSalary;
+    average -= minSalary;
+    average = average / (salaryLength - 2);
+
+    return average;
+        
+}
