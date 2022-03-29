495b802db518b3efcb7f46e2778811f0fcce6783
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-29 14:46:30 +0600

    LeetCode - Palindrome Number

---
+++ leetcode/PalindromeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine(Reverse(1239985));
+
+System.Console.WriteLine(Reverse(121));
+
+bool Reverse(int x) {
+    int result = 0;
+    int ox = x;
+    while (x > 0) {
+        int val = x % 10;
+        result += val;
+        x /= 10;
+        if (x > 0) {
+            result *= 10;
+        }
+    }
+
+    System.Console.WriteLine(result);
+
+    if (ox == result) {
+        return true;
+    } else {
+        return false;
+    }        
+}
+Console.WriteLine("Hello, World!");
