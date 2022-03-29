eb9e6315d8419b5922d172d345fc46d02d6eb5c8
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-29 14:30:30 +0600

    LeetCode - Reverse Integer

---
+++ leetcode/ReverseInteger/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine(Reverse(1239985));
+System.Console.WriteLine(Reverse(-120));
+System.Console.WriteLine(Reverse(2147483643));
+
+int Reverse(int x) {
+    int result = 0;
+
+    int x1;
+    if (x < 0) {
+        x1 = -x;
+    } else {
+        x1 = x;
+    }
+
+    while (x1 > 0) {
+        int val = x1 % 10;
+        if (result > (int.MaxValue - val)) {
+            return 0;
+        }
+        result += val;
+        x1 /= 10;
+        if (x1 > 0) {
+            if (result > (int.MaxValue / 10)) {
+                return 0;
+            } else {
+                result *= 10;
+            }
+        }
+    }
+
+    if (x < 0) {
+        return -result;
+    } else {
+        return result;
+    }
+        
+}
+Console.WriteLine("Hello, World!");
