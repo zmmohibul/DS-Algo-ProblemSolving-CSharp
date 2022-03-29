75a5235c3218f85f9612e02f6785646964aa8de4
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-29 15:52:34 +0600

    Longest Substring

---
+++ other-challenges/LongestSubstring/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// var s = "hellother";
+var s = "wednesday";
+
+var substringIndexDict = new Dictionary<Tuple<int, int>, int>();
+var containsDict = new Dictionary<char, int>();
+
+int i, j;
+int maxLength = 0;
+for (i = 0, j = 0; j < s.Length; j++) {
+    if (!containsDict.ContainsKey(s[j])) {
+        containsDict[s[j]] = j;
+    } else {
+        int length = j - i;
+        substringIndexDict[new Tuple<int, int>(i, j - 1)] = length;
+        if (length > maxLength) {
+            maxLength = length;
+        }
+        i += containsDict[s[j]] + 1;
+        containsDict[s[j]] = j;
+        maxLength = 0;
+    }
+
+}
+
+foreach (var (key, val) in substringIndexDict) {
+    System.Console.WriteLine($"{key} : {val}");
+}
+Console.WriteLine("Hello, World!");
