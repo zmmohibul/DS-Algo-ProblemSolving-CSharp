715037dc59620c872f722e366b14e2073276da57
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-29 16:43:56 +0600

    Longest Substring - 2

--- other-challenges/LongestSubstring/Program.cs
+++ other-challenges/LongestSubstring/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // var s = "hellother";
-var s = "wednesday";
+var s = "wednesdayin";
 
 var substringIndexDict = new Dictionary<Tuple<int, int>, int>();
 var containsDict = new Dictionary<char, int>();
@@ -20,7 +20,7 @@ for (i = 0, j = 0; j < s.Length; j++) {
         if (length > maxLength) {
             maxLength = length;
         }
-        i += containsDict[s[j]] + 1;
+        i = containsDict[s[j]] + 1;
         containsDict[s[j]] = j;
         maxLength = 0;
         ifirst = false;
@@ -28,7 +28,20 @@ for (i = 0, j = 0; j < s.Length; j++) {
 
 }
 
+// for (i = 0; i < s.Length; i++) {
+//     if (!containsDict.ContainsKey(s[i])) {
+//         containsDict[s[i]] = i;
+//     } else {
+//         break;
+//     }
+// }
 
+// substringIndexDict[new Tuple<int, int>(0, i - 1)] = i;
+
+// System.Console.WriteLine(i);
+
+
+substringIndexDict[new Tuple<int, int>(i, j - 1)] = i;
 
 foreach (var (key, val) in substringIndexDict) {
     System.Console.WriteLine($"{key} : {val}");
