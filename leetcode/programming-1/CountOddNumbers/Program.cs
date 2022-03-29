6a6d7c4d317bbe6a49561583b6e2d26e5f603e94
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-30 04:01:20 +0600

    P-1 - Count Odds

---
+++ leetcode/programming-1/CountOddNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine(CountOdds(1, 10));
+
+int CountOdds(int low, int high) {
+    int count = 0;
+    
+    for (int i = low; i <= high; i++) {
+        if (i % 2 != 0) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
