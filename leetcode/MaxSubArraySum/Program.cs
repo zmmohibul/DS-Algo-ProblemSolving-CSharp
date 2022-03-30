8758bb5ece3801c720543a48489f12f08975b1f0
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-30 08:45:54 +0600

    LeetCode - Max Sub Array Sum - 3

--- leetcode/MaxSubArraySum/Program.cs
+++ leetcode/MaxSubArraySum/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var nums = new int[] {-2, 1, -3, 4, -1, 2};
+var nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 MaxSubArray(nums);
 
 int MaxSubArray(int[] nums) {
@@ -19,9 +19,20 @@ int MaxSubArray(int[] nums) {
         }
     }
 
+    int max = 0;
+    Tuple<int, int> t1 = new Tuple<int, int>(0, 0);
+    
+
     foreach (var (k, v) in d) {
         System.Console.WriteLine($"[{k}: {v}],");
+        if (v > max) {
+            max = v;
+            t1 = k;
+        }
     }
+
+    System.Console.WriteLine(max);
+    System.Console.WriteLine(t1);
 
     return result;
 }
