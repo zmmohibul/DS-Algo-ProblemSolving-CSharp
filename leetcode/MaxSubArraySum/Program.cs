f7e55adaefe753a9d46e926fe454709f2372f6ef
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-30 08:40:40 +0600

    LeetCode - Max Sub Array Sum - 2

--- leetcode/MaxSubArraySum/Program.cs
+++ leetcode/MaxSubArraySum/Program.cs
@@ -4,23 +4,23 @@ MaxSubArray(nums);
 
 int MaxSubArray(int[] nums) {
     int result = 0;
-    var d = new Dictionary<int, Tuple<int, int>>();
+    var d = new Dictionary<Tuple<int, int>, int>();
 
     for (int i = 0; i < nums.Length; i++) {
         int sum = nums[i];
         for (int j = i; j < nums.Length; j++) {
             var t = new Tuple<int, int>(i, j);
             if (i == j) {
-                d[sum] = t;
+                d[t] = sum;
             } else {
                 sum += nums[j];
-                d[sum] = t;
+                d[t] = sum;
             }
         }
     }
 
     foreach (var (k, v) in d) {
-        System.Console.WriteLine($"[{v}: {k}],");
+        System.Console.WriteLine($"[{k}: {v}],");
     }
 
     return result;
