d6539e8682e0a8e6102be7858a407f6f2e7d60c4
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-30 10:07:43 +0600

    LeetCode - Max Sub Array Sum - Optimal 1

--- leetcode/MaxSubArraySum/Program.cs
+++ leetcode/MaxSubArraySum/Program.cs
@@ -3,28 +3,67 @@ var nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 System.Console.WriteLine(MaxSubArray(nums));
 
 
-nums = new int[] {5,4,-1,7,8};
-System.Console.WriteLine(MaxSubArray(nums));
+// nums = new int[] {5,4,-1,7,8};
+// System.Console.WriteLine(MaxSubArray(nums));
 
 
-nums = new int[] {5};
-System.Console.WriteLine(MaxSubArray(nums));
+// nums = new int[] {5};
+// System.Console.WriteLine(MaxSubArray(nums));
 
 int MaxSubArray(int[] nums) {
     // int result = 0;
     var d = new Dictionary<Tuple<int, int>, int>();
-
+    var sum = 0;
     for (int i = 0; i < nums.Length; i++) {
-        int sum = nums[i];
-        for (int j = i; j < nums.Length; j++) {
-            var t = new Tuple<int, int>(i, j);
-            if (i == j) {
-                d[t] = sum;
-            } else {
-                sum += nums[j];
-                d[t] = sum;
-            }
+        sum += nums[i];
+
+        var t1 = new Tuple<int, int>(0, i);
+        d[t1] = sum;
+
+        if (i + 1 < nums.Length) {
+            var s2 = nums[i] + nums[i + 1];
+            t1 = new Tuple<int, int>(i, i + 1);
+            d[t1] = s2;
         }
+
+        t1 = new Tuple<int, int>(i, i);
+        d[t1] = nums[i];
+
+        if (i - 1 > 0) {
+            var s1 = nums[i - 1] + nums[i];
+            t1 = new Tuple<int, int>(i - 1, i);
+            d[t1] = s1;
+        }
+
+
+        if (i - 1 > 0 && i + 1 < nums.Length) {
+            var s = nums[i - 1] + nums[i] + nums[i + 1];
+            t1 = new Tuple<int, int>(i - 1, i + 1);
+            d[t1] = s;
+        }
+
+        
+        
+
+        
+        
+
+
+
+
+
+
+
+
+        // for (int j = i; j < nums.Length; j++) {
+        //     var t = new Tuple<int, int>(i, j);
+        //     if (i == j) {
+        //         d[t] = sum;
+        //     } else {
+        //         sum += nums[j];
+        //         d[t] = sum;
+        //     }
+        // }
     }
 
     int max = nums[0];
@@ -32,7 +71,7 @@ int MaxSubArray(int[] nums) {
     
 
     foreach (var (k, v) in d) {
-        // System.Console.WriteLine($"[{k}: {v}],");
+        System.Console.WriteLine($"[{k}: {v}],");
         if (v > max) {
             max = v;
             // t1 = k;
@@ -41,6 +80,6 @@ int MaxSubArray(int[] nums) {
 
     // System.Console.WriteLine(max);
     // System.Console.WriteLine(t1);
-
+    System.Console.WriteLine(d.Count);
     return max;
 }
