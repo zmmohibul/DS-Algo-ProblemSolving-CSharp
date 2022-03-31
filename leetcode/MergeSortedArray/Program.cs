8c548ece6a3cf59f41b5c16950118396fe8f9650
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-31 16:13:21 +0600

    LeetCode - Merge Sorted Array - 2

--- leetcode/MergeSortedArray/Program.cs
+++ leetcode/MergeSortedArray/Program.cs
@@ -5,6 +5,12 @@ var nums2 = new int[] {6, 7, 12, 14};
 Merge(nums1, nums1.Length, nums2, nums2.Length);
 
 
+// nums1 = new int[] {1,2,3,0,0,0};
+// nums2 = new int[] {2,5,6};
+
+// Merge(nums1, 3, nums2, nums2.Length);
+
+
 void Merge(int[] nums1, int m, int[] nums2, int n) {
 
     int[] result = new int[nums1.Length];
@@ -14,7 +20,7 @@ void Merge(int[] nums1, int m, int[] nums2, int n) {
 
     int k = 0;
     while (k < nums1.Length) {
-        if (j < nums2.Length) {
+        if (j < n && i < m) {
             if (nums1[i] < nums2[j]) {
                 result[k] = nums1[i];
                 i += 1;
@@ -24,27 +30,31 @@ void Merge(int[] nums1, int m, int[] nums2, int n) {
                 j += 1;
                 k += 1;
             }
-        } else {
+        } else if (i < m) {
             result[k] = nums1[i];
             i += 1;
+            k += 1;
+        } else if (j < n) {
+            result[k] = nums2[j];
+            j += 1;
             k += 1;
         }
     }
 
     i = 0;
     foreach (var item in result) {
-        System.Console.Write(item + ", ");
+        // System.Console.Write(item + ", ");
         nums1[i] = item;
         i += 1;
     }
 
-    System.Console.WriteLine();
+    // System.Console.WriteLine();
 
-    foreach (var item in nums1) {
-        System.Console.Write(item + ", ");
-    }
-    
-    System.Console.WriteLine();
+    // foreach (var item in nums1) {
+    //     System.Console.Write(item + ", ");
+    // }
+
+    // System.Console.WriteLine();
 
         
 }
