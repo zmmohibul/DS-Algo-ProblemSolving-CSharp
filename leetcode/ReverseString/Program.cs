bbd81712a2acfa8e67d91797500a3ee1f5701789
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-01 09:58:53 +0600

    LeetCode - Reverse String

---
+++ leetcode/ReverseString/Program.cs
@@ -0,0 +1,16 @@
+﻿var s = new char[] {'h', 'e', 'l', 'l', 'o'};
+ReverseString(s);
+
+foreach(var item in s) {
+    System.Console.Write($"{item}, ");
+}
+
+
+void ReverseString(char[] s) {
+    int i, j;
+    for (i = 0, j = s.Length - 1; i < j; i++, j--) {
+        var c = s[j];
+        s[j] = s[i];
+        s[i] = c;
+    }
+}
