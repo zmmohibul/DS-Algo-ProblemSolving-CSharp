26d92a6fb100ec44dafb686e5df3ae0ee1126836
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-14 23:38:04 +0600

    LeetCode - Array - Richest Customer Wealth

---
+++ leetcode/Array/Easy/RichestCustomerWealth/Program.cs
@@ -0,0 +1,22 @@
+﻿int MaximumWealth(int[][] accounts) {
+    var maxWealth = 0;
+    var currentCustomerWealth = 0;
+
+    foreach (var customerAccount in accounts)
+    {
+        foreach (var amount in customerAccount) 
+        {
+            currentCustomerWealth += amount;
+        }
+
+        if (maxWealth < currentCustomerWealth) 
+        {
+            maxWealth = currentCustomerWealth;
+        }
+
+        currentCustomerWealth = 0;
+    }
+
+
+    return maxWealth;
+}
