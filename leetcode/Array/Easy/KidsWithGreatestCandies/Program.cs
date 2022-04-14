bba5c811bfe6c112e3ef8f087d4de4487d550fe9
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-15 02:06:38 +0600

    LeetCode - Array - Kids With Greatest Candies

---
+++ leetcode/Array/Easy/KidsWithGreatestCandies/Program.cs
@@ -0,0 +1,27 @@
+﻿IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+    var result = new List<bool>();
+    var kidWithMaxCandy = 0;
+
+    foreach (var candy in candies)
+    {
+        if (kidWithMaxCandy < candy)
+        {
+            kidWithMaxCandy = candy;
+        }
+    }
+
+    foreach (var candy in candies)
+    {
+        if (candy + extraCandies >= kidWithMaxCandy)
+        {
+            result.Add(true);
+        }
+        else
+        {
+            result.Add(false);
+        }
+    }
+
+    return result;
+        
+}
