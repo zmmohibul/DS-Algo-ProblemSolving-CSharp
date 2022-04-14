5cb0658d1ceca5c07ea78b3bd8bb473bfa817c2c
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-14 20:02:46 +0600

    LeetCode - Array - BuildArrayFromPermutation

---
+++ hackerrank/RoadRepair/Program.cs
@@ -0,0 +1,25 @@
+﻿long getMinCost(List<int> crew_id, List<int> job_id)
+{
+
+    long answer = 0;
+
+    crew_id.Sort();
+    job_id.Sort();
+
+
+    for (int i = 0; i < crew_id.Count; i++) 
+    {
+        if (crew_id[i] > job_id[i]) 
+        {
+            answer += crew_id[i] - job_id[i];
+        }
+        else 
+        {
+            answer += job_id[i] - crew_id[i];
+        }
+    }
+
+
+    return answer;
+
+}
---
+++ leetcode/Array/Easy/BuildArrayFromPermutation/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+int[] BuildArray(int[] nums) {
+    
+    var ans = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++) {
+        ans[i] = nums[nums[i]];
+    }
+    
+    return ans;
+    
+}
+
+var nums = new int[] {5,0,1,2,3,4};
+var ans = BuildArray(nums);
+ans.ToList().ForEach(item => Console.Write($"{item}, "));
+
