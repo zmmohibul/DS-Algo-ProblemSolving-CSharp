ea712304cbe8c4c63d828a14686d1b143c45fc1f
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-15 02:15:28 +0600

    LeetCode - Array - Numbers Smaller Than Current

---
+++ leetcode/Array/Easy/NumbersSmallerThanCurrent/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] SmallerNumbersThanCurrent(int[] nums) 
+{
+    var answer = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var currentNumber = nums[i];
+        var numbersSmallerThanCurrentCount = 0;
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] < currentNumber)
+            {
+                numbersSmallerThanCurrentCount += 1;
+            }
+
+        }
+
+        answer[i] = numbersSmallerThanCurrentCount;
+    }
+
+    return answer;
+}
