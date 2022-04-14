6de7bf73fdb28e9974acfe87ee0460537b2a2dbe
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-14 20:13:39 +0600

    LeetCode - Array - Running Sum of 1d Array

---
+++ leetcode/Array/Easy/RunningSumOf1dArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+int[] RunningSum(int[] arr)
+{
+    var result = new int[arr.Length];
+    var sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+        result[i] = sum;
+    }
+
+
+    return result;
+}
+
+var result = RunningSum(new int[] {1, 2, 3, 4});
+result.ToList().ForEach(item => Console.Write($"{item}, "));
