45ffde6a9d44ec236d548aed100544a7c5f48755
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-15 01:34:47 +0600

    LeetCode - Array - Number of Good Pairs

---
+++ leetcode/Array/Easy/NumberOfGoodPairs/Program.cs
@@ -0,0 +1,28 @@
+﻿int NumIdenticalPairs(int[] nums) 
+{
+    var numberOfGoodPairs = 0;
+
+    var dict = new Dictionary<int, int>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (!dict.ContainsKey(nums[i]))
+        {
+            dict[nums[i]] = 0;
+        }
+        else
+        {
+            dict[nums[i]] += 1;
+        }
+    }
+
+    foreach (var val in dict.Values)
+    {
+        numberOfGoodPairs += ((val * (val + 1)) / 2);
+    }
+
+    return numberOfGoodPairs;
+}
+
+System.Console.WriteLine(NumIdenticalPairs(new int[] {1,2,3,1,1,3}));
+System.Console.WriteLine(NumIdenticalPairs(new int[] {1,1,1,1}));
