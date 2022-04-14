16e6765dbb36e38da7e046870d29cf2885f35fa2
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-15 00:10:36 +0600

    LeetCode - Array - Final Value After Operation

---
+++ leetcode/Array/Easy/FindValueOfVariableAfterPerformingOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿int FinalValueAfterOperations(string[] operations) 
+{
+    int value = 0;
+
+    foreach (var operation in operations) 
+    {
+        if (operation.Equals("X++"))
+        {
+            value++;
+        }
+        else if (operation.Equals("++X"))
+        {
+            ++value;
+        }
+        else if (operation.Equals("--X"))
+        {
+            --value;
+        }
+        else if (operation.Equals("X--"))
+        {
+            value--;
+        }
+    }
+
+
+    return value;
+}
+
+var operations = new string[] {"--X","X++","X++"};
+System.Console.WriteLine(FinalValueAfterOperations(operations));
+
+operations = new string[] {"++X","++X","X++"};
+System.Console.WriteLine(FinalValueAfterOperations(operations));
