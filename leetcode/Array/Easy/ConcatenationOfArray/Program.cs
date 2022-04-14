890f3f963f91f30f07f1df726ceec5759efc2d24
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-14 20:09:15 +0600

    LeetCode - Array - ConcatenationOfArray

---
+++ leetcode/Array/Easy/ConcatenationOfArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+int[] GetConcatenation(int[] arr) 
+{
+    var ans = new int[arr.Length * 2];
+    var n = arr.Length;
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        ans[i] = arr[i];
+        ans[i + n] = arr[i];
+    }
+
+    return ans;
+}
+
+
+var nums = new int[] {1,3,2,1};
+var result = GetConcatenation(nums);
+
+result.ToList().ForEach(item => Console.Write($"{item}, "));
