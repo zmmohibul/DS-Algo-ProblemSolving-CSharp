75cbf2d5327de16f08c6463f07906b33c8b743c3
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-15 00:28:44 +0600

    LeetCode - Array - Most Words Found In Sentences

---
+++ leetcode/Array/Easy/MaxWordsInSentences/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int MostWordsFound(string[] sentences) 
+{
+    var maxWord = 0;
+
+    var currentSentenceWord = 0;
+
+    foreach(var sentence in sentences)
+    {
+        for (var i = 0; i < sentence.Length; i++)
+        {
+            if (sentence[i].Equals(' '))
+            {
+                currentSentenceWord += 1;
+            }
+        }
+
+        if (!String.IsNullOrEmpty(sentence))
+        {
+            currentSentenceWord += 1;
+        }
+
+        if (currentSentenceWord > maxWord)
+        {
+            maxWord = currentSentenceWord;
+        }
+
+        currentSentenceWord = 0;
+    }
+
+    return maxWord;
+}
+
+var sentences = new string[] {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+System.Console.WriteLine(MostWordsFound(sentences));
