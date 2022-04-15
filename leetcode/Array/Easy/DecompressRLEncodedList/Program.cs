883ded0ade208ac018a6ae3af39958a3f6e41256
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-15 17:34:36 +0600

    LeetCode - Array - Decompress RL Encoded List

---
+++ leetcode/Array/Easy/DecompressRLEncodedList/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] DecompressRLElist(int[] nums)
+{
+    var answer = new List<int>();
+
+    for (int i = 0; i < nums.Length; i += 2)
+    {
+        var freq = nums[i];
+        var val = nums[i + 1];
+
+        for (int j = 0; j < freq; j++)
+        {
+            answer.Add(val);
+        }
+    }
+
+
+    return answer.ToArray();
+        
+}
+
+var nums = new int[] {1,2,3,4};
+
+DecompressRLElist(nums).ToList().ForEach(num => Console.Write($"{num}, "));
