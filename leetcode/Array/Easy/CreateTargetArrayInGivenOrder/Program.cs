3bb813a5deaec4447efc43ca25ca7e8e3ac36da6
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-16 22:26:11 +0600

    Leetcode - Array - Create Target Array in Given Order

---
+++ leetcode/Array/Easy/CreateTargetArrayInGivenOrder/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] CreateTargetArray(int[] nums, int[] index) {
+    // traverse through index array and create a 
+    // Dictionary that counts how many times each index appear
+    int[] target = new int[index.Length];
+
+    var indexCountDict = new Dictionary<int, int>();
+    for (int i = 0; i < index.Length; i++)
+    {
+        if (!indexCountDict.ContainsKey(index[i]))
+        {
+            indexCountDict[index[i]] = 1;
+        }
+        else 
+        {
+            indexCountDict[index[i]] += 1;
+        }
+    }
+
+    var whereToStartIndex = new Dictionary<int, int>();
+    foreach (var item in index)
+    {
+        var startPos = 0;
+        for (int i = 0; i <= item; i++)
+        {
+            if (indexCountDict.ContainsKey(i))
+            {
+                startPos += indexCountDict[i];
+            }
+        }
+        whereToStartIndex[item] = startPos;
+    }
+
+    for (int i = 0; i < nums.Length; i++)  
+    {
+        var placeToInsertInTarget = whereToStartIndex[index[i]];
+        target[placeToInsertInTarget - 1] = nums[i];
+        whereToStartIndex[index[i]] -= 1;
+    }
+
+    return target;     
+}
+
+// CreateTargetArray(new int[]{0,1,2,3,4,5,6,7,8}, new int[]{0,1,2,2,1,3,1,3,2});
+CreateTargetArray(new int[]{0,1,2,3,4}, new int[]{0,1,2,2,1});
---
+++ leetcode/Array/Easy/ShuffleString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+string RestoreString(string s, int[] indices) {
+    var sb = new StringBuilder(s);
+
+    for (int i = 0; i < indices.Length; i++)
+    {
+        sb[indices[i]] = s[i];
+    }
+
+
+    return sb.ToString();
+        
+}
+
+System.Console.WriteLine(RestoreString("codeleet", new int[]{4,5,6,7,0,2,1,3}));
+System.Console.WriteLine(RestoreString("codeeelt", new int[]{4,5,6,7,2,1,0,3}));
