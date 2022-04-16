ccbd71fafa6db3fb664f565c59fe355909d5c80e
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-17 00:42:16 +0600

    Leetcode - Array - Design an Ordered Stream

---
+++ leetcode/Array/Easy/DesignOrderedStream/Program.cs
@@ -0,0 +1,51 @@
+﻿OrderedStream os = new OrderedStream(5);
+os.Insert(3, "ccccc").ToList().ForEach(item => Console.Write($"-- {item}, "));
+System.Console.WriteLine();
+
+os.Insert(1, "aaaaa").ToList().ForEach(item => Console.Write($"-- {item}, "));
+System.Console.WriteLine();
+
+os.Insert(2, "bbbbb").ToList().ForEach(item => Console.Write($"-- {item}, "));
+System.Console.WriteLine();
+
+os.Insert(5, "eeeee").ToList().ForEach(item => Console.Write($"-- {item}, "));
+System.Console.WriteLine();
+
+os.Insert(4, "ddddd").ToList().ForEach(item => Console.Write($"-- {item}, "));
+System.Console.WriteLine();
+
+
+public class OrderedStream 
+{
+    private string[] arr;
+    private int ptr = 0;
+
+    public OrderedStream(int n) 
+    {
+        this.arr = new string[n];
+    }
+    
+    public IList<string> Insert(int idKey, string value) 
+    {
+        arr[idKey - 1] = value;
+        
+        var listToReturn = new List<string>();
+
+        if (idKey - 1 == ptr)
+        {
+            var tempPtr = ptr;
+            while (tempPtr < arr.Length)
+            {
+                listToReturn.Add(arr[tempPtr]);
+                tempPtr += 1;
+                if (tempPtr >= arr.Length || arr[tempPtr] == null)
+                {
+                    break;
+                }
+            }
+        }
+
+        ptr += listToReturn.Count;
+        return listToReturn;
+    }
+}
