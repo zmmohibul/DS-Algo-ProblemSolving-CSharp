da78429954da88255c79558b1f8a5b0f09e19390
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-16 22:27:15 +0600

    Leetcode - Array - Count Items Matching Rule

---
+++ leetcode/CountItemsMatchinRule/Program.cs
@@ -0,0 +1,31 @@
+﻿int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue) 
+{
+    var count = 0;
+
+    var ruleKeyIndex = 0;
+
+    if (ruleKey.Equals("type"))
+    {
+        ruleKeyIndex = 0;
+    }
+    else if (ruleKey.Equals("color"))
+    {
+        ruleKeyIndex = 1;
+    }
+    else if (ruleKey.Equals("name"))
+    {
+        ruleKeyIndex = 2;
+    }
+
+    foreach (var item in items)
+    {
+        if (item[ruleKeyIndex].Equals(ruleValue))
+        {
+            count += 1;
+        }
+    }
+
+
+    return count;
+        
+}
