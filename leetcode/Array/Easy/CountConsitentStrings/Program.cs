ebaab1266c478ca55d2a08d861b8a6e2bbca7299
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-17 19:30:11 +0600

    Leetcode - Array - Count Consitent Strings

---
+++ leetcode/Array/Easy/CountConsitentStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿int CountConsistentStrings(string allowed, string[] words) 
+{
+    var allowedChars = new Dictionary<char, bool>();
+    foreach (var c in allowed)
+    {
+        if (!allowedChars.ContainsKey(c))
+        {
+            allowedChars[c] = true;
+        }
+    }
+
+    var count = 0;
+    foreach (var word in words)
+    {
+        bool isWordConsistent = true;
+        foreach (var c in word)
+        {
+            if (!allowedChars.ContainsKey(c))
+            {
+                isWordConsistent = false;
+                break;
+            }
+        }
+
+        if (isWordConsistent)
+        {
+            count += 1;
+        }
+    }
+
+    return count;
+}
