e349d7ab5ee3e17a820f973dac48b2135722cc0e
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-17 17:16:02 +0600

    Leetcode - Array - Count Number Of Pairs With K Difference

---
+++ leetcode/Array/Easy/CountNumberOfPairsWithKDiff/Program.cs
@@ -0,0 +1,17 @@
+﻿int CountKDifference(int[] nums, int k) 
+{
+    var count = 0;
+
+    for (int i = 0; i < nums.Length - 1; i++)        
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (Math.Abs(nums[i] - nums[j]) == k)
+            {
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
