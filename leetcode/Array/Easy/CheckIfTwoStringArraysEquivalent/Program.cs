31ff4cc4f28bbbef5a1195c630febffec49605b7
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-17 17:33:23 +0600

    Leetcode - Array - Check If Two String Arrays are Equivalent

---
+++ leetcode/Array/Easy/CheckIfTwoStringArraysEquivalent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+string[] strArr = {"AB", "BC", "CD", "DE", "EF", "FG", "GH", "IJ" };
+string str = string.Join("", strArr);
+System.Console.WriteLine(str);
+
+
+bool ArrayStringsAreEqual(string[] word1, string[] word2) 
+{
+    var word1Sb = new StringBuilder(string.Join("", word1));
+    var word2Sb = new StringBuilder(string.Join("", word2));
+
+    if (word1Sb.Equals(word1Sb))
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
