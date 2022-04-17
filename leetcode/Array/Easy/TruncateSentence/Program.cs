0a36ca630757e9e5b520f621a9a48a9ab87146bd
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-17 19:40:19 +0600

    Leetcode - Array - Truncate Sentence

---
+++ leetcode/Array/Easy/TruncateSentence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+string TruncateSentence(string s, int k) 
+{
+    var sb = new StringBuilder();
+
+    var count = 0;
+    var i = 0;
+    while (i < s.Length)
+    {
+        if (s[i].Equals(' '))
+        {
+            count += 1;
+        }
+        if (count == k)
+        {
+            break;
+        }
+        sb.Append(s[i]);
+        i += 1;
+    }
+
+    return sb.ToString();
+        
+}
