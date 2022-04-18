a74fd2057e590ef3e2ce1984a4f1a5da4ffaa2ee
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-19 01:37:31 +0600

    Leetcode - Array - MaxProductDifference

---
+++ leetcode/Array/Easy/MaximumProductDifferenceBetweenTwoPairs/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = new int[] {5,6,2,7,4};
+System.Console.WriteLine(MaxProductDifference(nums));
+
+int MaxProductDifference(int[] nums)
+{
+    Array.Sort(nums);
+
+    int max1 = nums[nums.Length - 1];
+    int max2 = nums[nums.Length - 2];
+
+    int min1 = nums[0];
+    int min2 = nums[1];
+        
+    return (max1 * max2) - (min1 * min2);
+}   
