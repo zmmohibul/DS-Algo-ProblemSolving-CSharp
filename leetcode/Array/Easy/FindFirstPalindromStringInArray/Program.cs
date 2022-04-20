7cc71c7d53d3753a4aa094ea6f96f58d6fff1dfc
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-20 22:02:40 +0600

    Leetcode - Array - Find First Palindrom String in Array

---
+++ leetcode/Array/Easy/FindFirstPalindromStringInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine(FirstPalindrome(new string[]{"abc","car","ada","racecar","cool"}));
+
+
+string FirstPalindrome(string[] words) 
+{
+    foreach (var word in words)
+    {
+        if (isPalindrome(word))
+        {
+            return word;
+        }
+    }
+    return "";
+}
+
+bool isPalindrome(string str)
+{
+    for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+    {
+        if (!str[i].Equals(str[j]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
