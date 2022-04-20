58a49ff5f6e72c2a622db743f6351324ce50c03e
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-21 00:47:57 +0600

    Leetcode - Array - Minimum Time Visiting All Points - 3

--- leetcode/Array/Easy/MinimumTimeVisitingAllPoints/Program.cs
+++ leetcode/Array/Easy/MinimumTimeVisitingAllPoints/Program.cs
@@ -1,79 +1,69 @@
 ﻿var list = new List<List<int>>();
 list.Add(new List<int>{1, 1});
-list.Add(new List<int>{3, 4});
+list.Add(new List<int>{4, 3});
 list.Add(new List<int>{-1, 0});
 
-// var points = new int[3][2];
-
-// points[0] = new int[] {1, 1};
-// points[1] = new int[] {3, 4};
-// points[2] = new int[] {-1, 0};
 System.Console.WriteLine(MinTimeToVisitAllPoints(list));
 
 
 
 int MinTimeToVisitAllPoints(List<List<int>> points) 
 {
-    var point1 = points[0];
     var time = 0;
+    var point1 = points[0];
+
     for (int i = 1; i < points.Count; i++)
     {
         var point2 = points[i];
-
-        if (point2[0] > point1[0] || point2[1] > point1[1])
+        if (point2[0] >= point1[0] && point2[1] >= point1[1])
         {
-            while (point1[0] != point2[0] || point1[1] != point2[1])
+            if (point2[0] < point2[1])
             {
-                if (point2[0] > point1[0] && point2[1] > point1[1])
-                {
-                    point1[0] += 1;
-                    point1[1] += 1;
-                    time += 1;
-                    continue;
-                }
-
-                if (point2[0] > point1[0])
-                {
-                    point1[0] += 1;
-                    time += 1;
-                }
-
-                if (point2[1] > point1[1])
-                {
-                    point1[1] += 1;
-                    time += 1;
-                }
+                var dt = point2[0] - point1[0];
+                time += dt;
+                point1[0] += dt;
+                point1[1] += dt;
+                dt = point2[1] - point1[1];
+                time += dt;
+                point1[1] += dt;
+            }
+            else
+            {
+                var dt = point2[1] - point1[1];
+                time += dt;
+                point1[0] += dt;
+                point1[1] += dt;
+                dt = point2[0] - point1[0];
+                time += dt;
+                point1[0] += dt;
             }
         }
-        else 
-        {
-            while (point1[0] != point2[0] || point1[1] != point2[1])
-            {
-                if (point2[0] < point1[0] && point2[1] < point1[1])
-                {
-                    point1[0] -= 1;
-                    point1[1] -= 1;
-                    time += 1;
-                    continue;
-                }
-
-                if (point2[0] < point1[0])
-                {
-                    point1[0] -= 1;
-                    time += 1;
-                }
-
-                if (point2[1] < point1[1])
-                {
-                    point1[1] -= 1;
-                    time += 1;
-                }
-            }
-
-        }
-
-        point1 = point2;
     }
 
+
     return time;
+}
+
+int GetMin(int n1, int n2)
+{
+    if (n1 < n2) 
+    {
+        return n1;
+    }
+    else 
+    {
+        return n2;
+    }
+}
+
+int GetMax(int n1, int n2)
+{
+    if (n1 > n2) 
+    {
+        return n1;
+    }
+    else 
+    {
+        return n2;
+    }
 }
