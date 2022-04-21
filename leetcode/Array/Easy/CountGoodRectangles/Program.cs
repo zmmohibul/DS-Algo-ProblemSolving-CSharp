939e1ab21c09dc95776f249386ccaed549615751
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-21 21:38:54 +0600

    Leetcode - Array - Count Good Rectangles

---
+++ leetcode/Array/Easy/CountGoodRectangles/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int CountGoodRectangles(int[][] rectangles) 
+{
+    int maxLen = 0;
+    int maxLenCount = 0;
+
+    foreach (var rectangle in rectangles)
+    {
+        int minSide = GetMinOfTwoInt(rectangle[0], rectangle[1]);
+        if (minSide > maxLen)
+        {
+            maxLen = minSide;
+            maxLenCount = 1;
+        }
+        else if (minSide == maxLen)
+        {
+            maxLenCount += 1;
+        }
+    }
+
+    return maxLenCount;
+}   
+
+int GetMinOfTwoInt(int n1, int n2)
+{
+    if (n1 < n2)
+    {
+        return n1;
+    }
+    else
+    {
+        return n2;
+    }
+}
