de326978ea98ba0adffb36523740900bb2472553
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-21 21:27:52 +0600

    Leetcode - Array - Find Highest Altitude

---
+++ leetcode/Array/Easy/FindHighestAltitude/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine(LargestAltitude(new int[] {-5,1,5,0,-7}));
+System.Console.WriteLine(LargestAltitude(new int[] {-4,-3,-2,-1,4,3,2}));
+
+
+int LargestAltitude(int[] gain) 
+{
+    int highestAltitude = 0;
+
+    var currentAltitude = 0;
+    for (int i = 0; i < gain.Length; i++)
+    {
+        currentAltitude += gain[i];
+        if (currentAltitude > highestAltitude)
+        {
+            highestAltitude = currentAltitude;
+        }
+    }
+
+
+    return highestAltitude;
+}
