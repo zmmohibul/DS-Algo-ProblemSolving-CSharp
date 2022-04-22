a475df65b2c016b3ae345f0f5dd2297dca56f51f
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-23 05:58:56 +0600

    Leetcode - Array - Max Product of Two Elements in Array

---
+++ leetcode/Array/Easy/MaxProductOfTwoElementsInArray/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine(MaxProduct(new int[] {3,4,5,2}));
+
+int MaxProduct(int[] nums) 
+{
+    int maxProduct = 0;
+
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if ((nums[i] - 1) * (nums[j] - 1) > maxProduct)
+            {
+                maxProduct = (nums[i] - 1) * (nums[j] - 1);
+            }
+        }
+    }
+
+    return maxProduct;
+}
