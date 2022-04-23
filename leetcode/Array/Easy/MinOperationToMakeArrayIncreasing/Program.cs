fd4cd085a49af88365f79a55de953e1fc1e9ef40
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-23 06:13:44 +0600

    Leetcode - Array - Min Operation To Make Array Increasing

---
+++ leetcode/Array/Easy/MinOperationToMakeArrayIncreasing/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine(MinOperations(new int[]{1,5,2,4,1}));
+
+int MinOperations(int[] nums)
+ {
+        int operationCount = 0;
+        for (int i = 0; i < nums.Length - 1; i++) {
+            if (nums[i + 1] <= nums[i]) {
+                int iplus1Increment = nums[i] - nums[i + 1] + 1;
+                operationCount += iplus1Increment;
+                nums[i + 1] += iplus1Increment;
+                    
+            }
+        }
+        
+        return operationCount;
+}
