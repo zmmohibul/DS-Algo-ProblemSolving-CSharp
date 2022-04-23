21e0594a8edc894b9dab0a5e60375dc3f62123e5
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-24 00:21:35 +0600

    Leetcode - Array - Find N Unique Integers Sum up to Zero

---
+++ leetcode/Array/Easy/FindNUniqueIntegersSumUpToZero/Program.cs
@@ -0,0 +1,20 @@
+﻿SumZero(5).ToList().ForEach(item => System.Console.Write($"{item}, "));
+
+int[] SumZero(int n) 
+{
+    int halfN = n / 2;
+    int[] arr = new int[n];
+    int i = 0;
+    while (i < n - 1)
+    {
+        arr[i] = halfN;
+        arr[i + 1] = -halfN;
+        i += 2;
+        halfN -= 1;
+    }
+    if (n % 2 != 0)
+    {
+        arr[n - 1] = 0;
+    }
+    return arr;
+}
