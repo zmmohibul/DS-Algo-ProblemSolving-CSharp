62036d04a6d0a8069dbbbabf6a4c03fb794e3e56
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-23 06:58:01 +0600

    Leetcode - Array - Find Number with Even Number of Digits

---
+++ leetcode/Array/Easy/FindNumbersWithEvenNumberDigits/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine(FindNumbers(new int[] {12,345,2,6,7896}));
+int FindNumbers(int[] nums)
+{
+    int count = 0;
+    foreach (int num in nums)
+    {
+        int numberOfDigits = 0;
+        int tempNum = num;
+        while (tempNum > 0)
+        {
+            numberOfDigits += 1;
+            tempNum /= 10;
+        }
+        
+        if (numberOfDigits % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    
+    return count;
+    
+}
