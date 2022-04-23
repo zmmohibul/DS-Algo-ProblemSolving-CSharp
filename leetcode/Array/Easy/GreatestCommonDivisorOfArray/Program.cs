a52264fce571200e099eb6167b67e750682472b2
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-23 06:35:59 +0600

    Leetcode - Array - Find GCD of Array

---
+++ leetcode/Array/Easy/GreatestCommonDivisorOfArray/Program.cs
@@ -0,0 +1,39 @@
+﻿System.Console.WriteLine(FindGCD(new int[] {2,5,6,9,10}));
+System.Console.WriteLine(FindGCD(new int[] {7,5,6,8,3}));
+System.Console.WriteLine(FindGCD(new int[] {3,3}));
+
+
+
+int FindGCD(int[] nums) 
+{
+    int gcd = 0;
+    int min = int.MaxValue;
+    int max = int.MinValue;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] > max)
+        {
+            max = nums[i];
+        }
+
+        if (nums[i] < min) 
+        {
+            min = nums[i];
+        }
+    }
+
+    while (true)
+    {
+        if (min == 0 || max == 0)
+        {
+            break;
+        }
+
+        int rem = max % min;
+        max = min;
+        min = rem;
+    }
+
+    return max;
+}
