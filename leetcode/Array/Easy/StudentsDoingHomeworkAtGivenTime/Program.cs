e45a5bee53f76675cb6a7c21656d808cc292a223
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-24 01:43:38 +0600

    Leetcode - Array - Students Doing Homework At Given Time

---
+++ leetcode/Array/Easy/StudentsDoingHomeworkAtGivenTime/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine(BusyStudent(new int[] {1,2,3}, new int[] {3,2,7}, 4));
+
+int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+{
+    int noOfStudents = 0;
+    for (int i = 0; i < startTime.Length; i++)
+    {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+        {
+            noOfStudents += 1;
+        }
+    }
+    return noOfStudents;
+}
