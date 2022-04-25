83673ddcf97cfb0aae1c2a8af3b63a809e1227cf
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-26 02:02:17 +0600

    MiniatureGit - Add - save file with sha

--- MiniatureGit/Repository.cs
+++ MiniatureGit/Repository.cs
@@ -1,12 +1,16 @@
+using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MiniatureGit
 {
     public class Repository
     {
-        private static DirectoryInfo CURR_DIR = new DirectoryInfo(".");
-        private static DirectoryInfo MINIATURE_GIT = new DirectoryInfo(Path.Join(CURR_DIR.FullName, ".minigit"));
-        private static DirectoryInfo BLOOBS = new DirectoryInfo(Path.Join(MINIATURE_GIT.FullName, "bloobs"));
+        private static DirectoryInfo PWD = new DirectoryInfo(".");
+        private static DirectoryInfo MINIATURE_GIT = new DirectoryInfo(Path.Join(PWD.FullName, ".minigit"));
+        private static DirectoryInfo BLOBS = new DirectoryInfo(Path.Join(MINIATURE_GIT.FullName, "blobs"));
         public static void Init()
         {
             if (MINIATURE_GIT.Exists)
@@ -15,9 +19,26 @@ namespace MiniatureGit
                 System.Environment.Exit(1);
             }
 
-            System.Console.WriteLine(CURR_DIR.FullName);
+            System.Console.WriteLine(PWD.FullName);
             MINIATURE_GIT.Create();
         }
+
+        public static async Task Add(string FileName)
+        {
+            BLOBS.Create();
+            byte[] textInFile = await File.ReadAllBytesAsync(Path.Join(PWD.FullName, FileName));
+            System.Console.WriteLine(textInFile);
+            System.Console.WriteLine(Hash(textInFile));
+            await File.WriteAllBytesAsync(Path.Join(BLOBS.FullName, Hash(textInFile)), textInFile);
+        }
+
+
+        private static string Hash(byte[] input)
+        {
+            using var sha1 = SHA1.Create();
+            return Convert.ToHexString(sha1.ComputeHash(input));
+        }
+
 
     }
 }
