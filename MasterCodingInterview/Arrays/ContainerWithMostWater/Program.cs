852601a29d13398d7afb47a140590efa5e46f81f
zmmohibul <18-36427-1@student.aiub.edu>
2022-06-27 13:53:43 +0600

    Container With Most Water - Brute Force

---
+++ MasterCodingInterview/Arrays/ContainerWithMostWater/Program.cs
@@ -0,0 +1,20 @@
+﻿int MaxArea(int[] height) 
+{
+    int maxArea = 0;
+
+    for (int i = 0; i < height.Length - 1; i++)
+    {
+        for (int j = 0; j < height.Length; j++)
+        {
+            var jCurrentArea = (j - i) * (height[i] < height[j] ? height[i] : height[j]);
+            if (jCurrentArea > maxArea)
+            {
+                maxArea = jCurrentArea;
+            }
+        }
+    }
+
+    return maxArea;   
+}
+
+System.Console.WriteLine(MaxArea(new int[] {1,8,6,2,5,4,8,3,7}));
