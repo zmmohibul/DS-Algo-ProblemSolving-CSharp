b1b630d5695a2dfef8cc15403533c8f1c8bdc94d
zmmohibul <18-36427-1@student.aiub.edu>
2022-06-27 11:13:33 +0600

    leetcode - CountEqualDivisiblePairsInArray - optimized 1

--- leetcode/Array/Easy/CountEqualDivisiblePairsInArray/Program.cs
+++ leetcode/Array/Easy/CountEqualDivisiblePairsInArray/Program.cs
@@ -1,15 +1,42 @@
 ﻿int CountPairs(int[] nums, int k) {
     int count = 0;
-
-    for (int i = 0; i < nums.Length - 1; i++)
+    var dict = new Dictionary<int, List<int>>();
+    for (int i = 0; i < nums.Length; i++)
     {
-        for (int j = 1; j < nums.Length; j++)
+        if (!dict.ContainsKey(nums[i]))
         {
-            if ((nums[i] == nums[j]) && ((i * j) % k == 0))
+            dict[nums[i]] = new List<int>();
+        }
+        else
+        {
+            foreach (var index in dict[nums[i]])
             {
-                count += 1;
+                if ((i * index) % k == 0)
+                {
+                    count += 1;
+                }
             }
         }
+        dict[nums[i]].Add(i);
+        // for (int j = i+1; j < nums.Length; j++)
+        // {
+        //     if ((nums[i] == nums[j]) && ((i * j) % k == 0))
+        //     {
+        //         count += 1;
+        //     }
+        // }
     }
+    // foreach (var (num, indices) in dict)
+    // {
+    //     Console.Write($"{num}: ");
+    //     foreach (var index in indices)
+    //     {
+    //         Console.Write($"{index} ");
+    //     }
+    //     System.Console.WriteLine();
+    // }
+    // System.Console.WriteLine();
     return count;
 }
+
+System.Console.WriteLine(CountPairs(new int[] {3,1,2,2,2,1,3}, 2));;
