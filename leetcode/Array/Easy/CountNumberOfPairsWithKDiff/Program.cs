e326e692e8d2ed0561e3ad11b4d4816120e3941a
zmmohibul <18-36427-1@student.aiub.edu>
2022-06-27 10:49:31 +0600

    leetcode - CountNumberOfPairsWithKDiff optimized

--- leetcode/Array/Easy/CountNumberOfPairsWithKDiff/Program.cs
+++ leetcode/Array/Easy/CountNumberOfPairsWithKDiff/Program.cs
@@ -1,17 +1,50 @@
 ﻿int CountKDifference(int[] nums, int k) 
 {
     var count = 0;
-
-    for (int i = 0; i < nums.Length - 1; i++)        
+    var numsFreq = new Dictionary<int, int>();
+    foreach (var num in nums)
     {
-        for (int j = i + 1; j < nums.Length; j++)
+        if (!numsFreq.ContainsKey(num))
         {
-            if (Math.Abs(nums[i] - nums[j]) == k)
-            {
-                count += 1;
-            }
+            numsFreq[num] = 1;
         }
+        else
+        {
+            numsFreq[num] += 1;
+        }
+    }
+
+    foreach (var (key, value) in numsFreq)
+    {
+        Console.Write($"{key}: {value}, ");
+    }
+    System.Console.WriteLine();
+
+    foreach (var num in nums)
+    {
+        var diff1 = num + k;
+        var diff2 = num - k;
+        Console.WriteLine($"{num}: ({diff1}, {diff2})");
+
+        if (numsFreq.ContainsKey(diff1))
+        {
+            count += numsFreq[diff1];
+        }
+
+        if (numsFreq.ContainsKey(diff2))
+        {
+            count += numsFreq[diff2];
+        }
+
+        numsFreq[num] -= 1;
+        foreach (var (key, value) in numsFreq)
+        {
+            Console.Write($"{key}: {value}, ");
+        }
+        System.Console.WriteLine();
     }
 
     return count;
 }
+
+System.Console.WriteLine(CountKDifference(new int[] {3, 2, 1, 5, 4}, 2));
