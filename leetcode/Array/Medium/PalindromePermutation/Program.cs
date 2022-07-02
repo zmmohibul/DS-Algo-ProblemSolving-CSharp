ef04f08c943a10b2c540207c8f01f26975a376d2
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-02 18:02:56 +0600

    LeetCode - Palindrome Permutation

---
+++ leetcode/Array/Medium/PalindromePermutation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalindromePermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ leetcode/Array/Medium/PalindromePermutation/Solution.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalindromePermutation
+{
+    public class Solution
+    {
+        public bool CanPermutePalindrome(string s) 
+        {
+            Dictionary<char, int> charFreq = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!charFreq.ContainsKey(s[i]))
+                {
+                    charFreq[s[i]] = 1;
+                }
+                else
+                {
+                    charFreq[s[i]] += 1;
+                }
+            }
+
+            if (s.Length % 2 == 0)
+            {
+                foreach (var (ch, count) in charFreq)
+                {
+                    if (count % 2 != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                var unevenChars = 0;
+                foreach (var (ch, count) in charFreq)
+                {
+                    if (count % 2 != 0)
+                    {
+                        unevenChars += 1;
+                    }
+                    if (unevenChars > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
