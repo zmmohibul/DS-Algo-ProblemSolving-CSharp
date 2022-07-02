c382318adf8e9f032a3a7d6ce4c50fb06b55449e
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-03 00:56:14 +0600

    LeetCode - LinkedList RemoveNthNodeFromEndOfList - 1

---
+++ leetcode/LinkedList/Medium/RemoveNthNodeFromEndOfList/ListNode.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoveNthNodeFromEndOfList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) 
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ leetcode/LinkedList/Medium/RemoveNthNodeFromEndOfList/Solution.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoveNthNodeFromEndOfList
+{
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n) 
+        {
+            if (head == null || head.next == null)
+            {
+                return null;
+            }
+
+            var curr = head;
+            var count = 0;
+            while (curr != null)
+            {
+                count += 1;
+                curr = curr.next;
+            }
+
+            var nIndex = count - n;
+            curr = head;
+            for (int i = 1; i < nIndex; i++)
+            {
+                curr = curr.next;
+            }
+
+            if (curr.next.next != null)
+            {
+                curr.next = curr.next.next;
+            }
+            else
+            {
+                curr.next = null;
+            }
+
+            return head;
+        }
+    }
+}
