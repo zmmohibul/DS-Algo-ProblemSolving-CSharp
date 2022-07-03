520f63395e669cf258a52a15bc7047048e664d4c
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-04 03:13:44 +0600

    LeetCode - Array - IntersectionOfThreeSortedArrays O(n log n)

--- leetcode/Array/Easy/IntersectionOfThreeSortedArrays/Program.cs
+++ leetcode/Array/Easy/IntersectionOfThreeSortedArrays/Program.cs
@@ -6,7 +6,8 @@ namespace IntersectionOfThreeSortedArrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var s = new Solution();
+            System.Console.WriteLine(s.BinarySearch(new int[] {12, 28, 32, 46, 50, 88, 91, 94, 98}, 32));
         }
     }
 }
--- leetcode/Array/Easy/IntersectionOfThreeSortedArrays/Solution.cs
+++ leetcode/Array/Easy/IntersectionOfThreeSortedArrays/Solution.cs
@@ -9,35 +9,20 @@ namespace IntersectionOfThreeSortedArrays
     {
         public IList<int> ArraysIntersection(int[] arr1, int[] arr2, int[] arr3) 
         {
-            // Brute Force O(n^2)
+            // Brute Force O(n log n)
             // arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
             List<int> result = new List<int>();
             // Iterate over arr1 with index i
             for (int i = 0; i < arr1.Length; i++)
             {
                 int ithElementArr1 = arr1[i];
+                
             
-                // check if arr1[i] exist in arr2
-                bool ithElementArr1ExistInArr2 = false;
-                for (int j = 0; j < arr2.Length; j++)
-                {
-                    if (arr2[j] == ithElementArr1)
-                    {
-                        ithElementArr1ExistInArr2 = true;
-                        break;
-                    }
-                }
+                // check if arr1[i] exist in arr2 using binary search
+                bool ithElementArr1ExistInArr2 = BinarySearch(arr2, ithElementArr1);
             
-                // check if arr1[i] exist in arr3
-                bool ithElementArr1ExistInArr3 = false;
-                for (int k = 0; k < arr3.Length; k++)
-                {
-                    if (arr3[k] == ithElementArr1)
-                    {
-                        ithElementArr1ExistInArr3 = true;
-                        break;
-                    }
-                }
+                // check if arr1[i] exist in arr3 using binary search
+                bool ithElementArr1ExistInArr3 = BinarySearch(arr3, ithElementArr1);
 
                 // if arr1[i] exist in arr2 and arr3, add arr1[i] to result
                 if (ithElementArr1ExistInArr2 && ithElementArr1ExistInArr3)
@@ -47,6 +32,31 @@ namespace IntersectionOfThreeSortedArrays
             }
             
             return result;
+        }
+
+        public bool BinarySearch(int[] arr, int target)
+        {
+            int i = 0;
+            int j = arr.Length - 1;
+            while (j > i)
+            {
+                int mid = (i + j) / 2;
+                if (arr[mid] == target)
+                {
+                    return true;
+                }
+
+                if (arr[mid] > target)
+                {
+                    j = mid - 1;
+                }
+                else if (arr[mid] < target)
+                {
+                    i = mid + 1;
+                }
+            }
+
+            return false;
         }
     }
 }
