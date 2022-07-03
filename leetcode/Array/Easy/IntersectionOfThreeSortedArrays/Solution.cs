fbde78a20c0b2e48b0d5ffd4cbf968bd0a0867a0
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-04 03:18:53 +0600

    LeetCode - Array - IntersectionOfThreeSortedArrays O(n)

--- leetcode/Array/Easy/IntersectionOfThreeSortedArrays/Solution.cs
+++ leetcode/Array/Easy/IntersectionOfThreeSortedArrays/Solution.cs
@@ -9,20 +9,34 @@ namespace IntersectionOfThreeSortedArrays
     {
         public IList<int> ArraysIntersection(int[] arr1, int[] arr2, int[] arr3) 
         {
-            // Brute Force O(n log n)
+            // Optimized O(n)
             // arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
             List<int> result = new List<int>();
+
+            // Build a HashSet from arr2
+            HashSet<int> arr2HashSet = new HashSet<int>();
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr2HashSet.Add(arr2[i]);
+            }
+
+            // Build a HashSet from arr3
+            HashSet<int> arr3HashSet = new HashSet<int>();
+            for (int i = 0; i < arr3.Length; i++)
+            {
+                arr3HashSet.Add(arr3[i]);
+            }
+
             // Iterate over arr1 with index i
             for (int i = 0; i < arr1.Length; i++)
             {
                 int ithElementArr1 = arr1[i];
                 
+                // check if arr1[i] exist in arr2HashSet
+                bool ithElementArr1ExistInArr2 = arr2HashSet.Contains(ithElementArr1);
             
-                // check if arr1[i] exist in arr2 using binary search
-                bool ithElementArr1ExistInArr2 = BinarySearch(arr2, ithElementArr1);
-            
-                // check if arr1[i] exist in arr3 using binary search
-                bool ithElementArr1ExistInArr3 = BinarySearch(arr3, ithElementArr1);
+                // check if arr1[i] exist in arr3Hashset
+                bool ithElementArr1ExistInArr3 = arr3HashSet.Contains(ithElementArr1);
 
                 // if arr1[i] exist in arr2 and arr3, add arr1[i] to result
                 if (ithElementArr1ExistInArr2 && ithElementArr1ExistInArr3)
