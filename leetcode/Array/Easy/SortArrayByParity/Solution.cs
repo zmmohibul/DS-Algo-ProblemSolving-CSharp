704d60d371734541418cb1519d6af60bbf90adc4
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-04 13:40:19 +0600

    LeetCode - Array - Sort Array By Parity Optimized

--- leetcode/Array/Easy/SortArrayByParity/Solution.cs
+++ leetcode/Array/Easy/SortArrayByParity/Solution.cs
@@ -10,25 +10,21 @@ namespace SortArrayByParity
         public int[] SortArrayByParity(int[] nums) 
         {
             List<int> odds = new List<int>();
-            List<int> evens = new List<int>();
+
+            int[] result = new int[nums.Length];
+            int rInd = 0;
+
             for (int i = 0; i < nums.Length; i++)
             {
                 if (nums[i] % 2 == 0)
                 {
-                    evens.Add(nums[i]);
+                    result[rInd] = nums[i];
+                    rInd += 1;
                 }
                 else
                 {
                     odds.Add(nums[i]);
                 }
-            }
-
-            int[] result = new int[nums.Length];
-            int rInd = 0;
-            for (int i = 0; i < evens.Count; i++)
-            {
-                result[rInd] = evens[i];
-                rInd += 1;
             }
 
             for (int i = 0; i < odds.Count; i++)
