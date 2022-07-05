26e584a1b9bcdd9d81576767d04dc1b854a96b3d
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-06 01:31:40 +0600

    N Repeated Element In Size 2N Array - O(n) better

--- leetcode/Array/Easy/NRepeatedElementInSize2NArray/Solution.cs
+++ leetcode/Array/Easy/NRepeatedElementInSize2NArray/Solution.cs
@@ -10,15 +10,11 @@ namespace NRepeatedElementInSize2NArray
         public int RepeatedNTimes(int[] nums)
         {
             var numFreq = new Dictionary<int, int>();
+            var numToReturn = 0;
             foreach (var num in nums)
             {
                 numFreq[num] = numFreq.GetValueOrDefault(num, 0) + 1;
-            }
-
-            var numToReturn = 0;
-            foreach (var (num, count) in numFreq)
-            {
-                if (count > 1)
+                if (numFreq[num] > 1)
                 {
                     numToReturn = num;
                     break;
