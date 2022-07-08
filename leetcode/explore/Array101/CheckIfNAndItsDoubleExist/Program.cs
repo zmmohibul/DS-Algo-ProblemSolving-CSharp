9d133f4fa588f5aa2bde463df17888a28f616a5e
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-08 15:11:14 +0600

    LeetCode - Array Explore - Check If N And Its Double Exist - O(N log N)

--- leetcode/explore/Array101/CheckIfNAndItsDoubleExist/Program.cs
+++ leetcode/explore/Array101/CheckIfNAndItsDoubleExist/Program.cs
@@ -11,6 +11,7 @@ namespace CheckIfNAndItsDoubleExist
             System.Console.WriteLine(sl.CheckIfExist(new int[] {10,2,5,3}));
             System.Console.WriteLine(sl.CheckIfExist(new int[] {7,1,14,11}));
             System.Console.WriteLine(sl.CheckIfExist(new int[] {3,1,7,11}));
+            // System.Console.WriteLine(sl.BinarySearch(new int[] {1, 2, 3, 4, 6, 9, 11, 22, 33}, 4.0, 0));
         }
     }
 }
--- leetcode/explore/Array101/CheckIfNAndItsDoubleExist/Solution.cs
+++ leetcode/explore/Array101/CheckIfNAndItsDoubleExist/Solution.cs
@@ -9,14 +9,40 @@ namespace CheckIfNAndItsDoubleExist
     {
         public bool CheckIfExist(int[] arr)
         {
+            Array.Sort(arr);
             for (int i = 0; i < arr.Length - 1; i++)
             {
-                for (int j = i + 1; j < arr.Length; j++)
+                if (BinarySearch(arr, arr[i] * 2, i + 1))
                 {
-                    if (arr[i] == 2 * arr[j] || arr[i] == arr[j] / 2.0)
-                    {
-                        return true;
-                    }
+                    return true;
+                }
+
+                if (BinarySearch(arr, arr[i] / 2.0, i + 1))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool BinarySearch(int[] arr, double target, int left)
+        {
+            int right = arr.Length - 1;
+            while (right >= left)
+            {
+                int mid = (right + left) / 2;
+                if (arr[mid] == target)
+                {
+                    return true;
+                }
+
+                if (target > arr[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
                 }
             }
             return false;
