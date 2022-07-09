5d2609991454961db2b9556490d1be94d2fad075
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-09 21:54:01 +0600

    LeetCode - Find Pivot Index - Works

--- leetcode/explore/ArrayAndString/FindPivotIndex/Solution.cs
+++ leetcode/explore/ArrayAndString/FindPivotIndex/Solution.cs
@@ -11,35 +11,39 @@ namespace FindPivotIndex
         {
             var iSumDict = new Dictionary<int, int>();
             var iSum = 0;
-            for (var i = 0; i < nums.Length - 1; i++)
+            var i = 0;
+            for (i = nums.Length - 1; i > 1; i--)
             {
                 iSum += nums[i];
                 iSumDict[i] = iSum;
             }
-            
+
+            iSum += nums[i];
             if (iSum == 0)
-            {
-                return nums.Length - 1;
-            }
-            
-            var jSum = 0;
-            var j = 0;
-            for (j = nums.Length - 1; j > 1; j--)
-            {
-                jSum += nums[j];
-                if (iSumDict[j - 2] == jSum)
-                {
-                    return j - 1;
-                }
-            }
-            
-            jSum += nums[j];
-            if (jSum == 0)
             {
                 return 0;
             }
-            
+
+            var jSum = 0;
+            var j = 0;
+            for (j = 0; j < nums.Length - 2; j++)
+            {
+                jSum += nums[j];
+                if (iSumDict[j + 2] == jSum)
+                {
+                    return j + 1;
+                }
+            }
+
+            jSum += nums[j];
+            if (jSum == 0)
+            {
+                return nums.Length - 1;
+            }
+
             return -1;
+
+            
         }
     }
 }
