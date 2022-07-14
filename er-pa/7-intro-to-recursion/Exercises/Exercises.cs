4f641638beafc1ce674309fc31423766f8f1d758
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-14 11:46:05 +0600

    er-pa - intro-to-rec - Digital Root

--- er-pa/7-intro-to-recursion/Exercises/Exercises.cs
+++ er-pa/7-intro-to-recursion/Exercises/Exercises.cs
@@ -7,6 +7,17 @@ namespace Exercises
 {
     public class Exercises
     {
+        public int DigitalRoot(int n)
+        {
+            if (n / 10 == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return DigitalRoot(DigitSum(n));
+            }
+        }
         public int DigitSum(int n)
         {
             if (n / 10 == 0)
--- er-pa/7-intro-to-recursion/Exercises/Program.cs
+++ er-pa/7-intro-to-recursion/Exercises/Program.cs
@@ -9,6 +9,7 @@ namespace Exercises
             Console.WriteLine("Hello World!");
             var es = new Exercises();
             System.Console.WriteLine(es.DigitSum(1729));
+            System.Console.WriteLine(es.DigitalRoot(1729));
         }
     }
 }
