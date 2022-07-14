3ceaa2efdf285d875ab869f30cd83447aeb09d03
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-14 11:51:35 +0600

    er-pa - intro-to-rec - String Reverse

--- er-pa/7-intro-to-recursion/Exercises/Exercises.cs
+++ er-pa/7-intro-to-recursion/Exercises/Exercises.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercises
 {
     public class Exercises
     {
+        public string StringReverse(string str)
+        {
+            var sb = new StringBuilder(str);
+            return StringReverse(sb, 0, sb.Length - 1);
+        }
+
+        public string StringReverse(StringBuilder sb, int p1, int p2)
+        {
+            if (p1 >= p2)
+            {
+                return sb.ToString();
+            }
+
+            var c = sb[p1];
+            sb[p1] = sb[p2];
+            sb[p2] = c;
+            return StringReverse(sb, p1 + 1, p2 - 1);
+        }
         public int DigitalRoot(int n)
         {
             if (n / 10 == 0)
--- er-pa/7-intro-to-recursion/Exercises/Program.cs
+++ er-pa/7-intro-to-recursion/Exercises/Program.cs
@@ -10,6 +10,7 @@ namespace Exercises
             var es = new Exercises();
             System.Console.WriteLine(es.DigitSum(1729));
             System.Console.WriteLine(es.DigitalRoot(1729));
+            System.Console.WriteLine(es.StringReverse("abcdef"));
         }
     }
 }
