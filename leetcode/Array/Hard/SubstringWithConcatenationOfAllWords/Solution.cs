7f3dd15f35f4fee2956153dc9e9af55f288a1ce4
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-18 02:27:07 +0600

    Substring with Concatenation of All Words - Works

--- leetcode/Array/Hard/SubstringWithConcatenationOfAllWords/Solution.cs
+++ leetcode/Array/Hard/SubstringWithConcatenationOfAllWords/Solution.cs
@@ -11,45 +11,45 @@ namespace SubstringWithConcatenationOfAllWords
         public IList<int> FindSubstring(string s, string[] words) 
         {
             var result = new List<int>();
-            for (int i = 0; i < words.Length; i++)
+            var sminlen = words.Length * words[0].Length;
+            for (int i = 0; i <= s.Length - sminlen; i++)
             {
-                var word = new StringBuilder(words[i]);
+                var d = new Dictionary<string, int>();
+                foreach (var word in words)
+                {
+                    d[word] = d.GetValueOrDefault(word, 0) + 1;
+                }
+
+                int k = i;
+                var found = true;
                 for (int j = 0; j < words.Length; j++)
                 {
-                    if (j != i)
+                    // var w = new StringBuilder();
+                    // for (int l = 0; l < words[0].Length; l++)
+                    // {
+                    //     w.Append(s[k]);
+                    //     k += 1;
+                        
+                    // }
+
+                    var ww = s.Substring(k, words[0].Length);
+                    System.Console.WriteLine(ww);
+                    k += words[0].Length;
+
+                    if (d.ContainsKey(ww) && d[ww] > 0)
                     {
-                        word.Append(words[j]);
+                        d[ww] -= 1;
+                    }
+                    else 
+                    {
+                        found = false;
+                        break;
                     }
                 }
-                System.Console.WriteLine(word);
 
-                var subS = new StringBuilder();
-                for (int j = 0; j < word.Length; j++)
+                if (found)
                 {
-                    subS.Append(s[j]);
-                }
-                // System.Console.WriteLine(subS);
-
-                if (subS.Equals(word))
-                {
-                    result.Add(0);
-                }
-
-                int k = 0;
-                int l = word.Length;
-                while (l < s.Length)
-                {
-                    subS.Remove(0, 1);
-                    subS.Append(s[l]);
-                    // System.Console.WriteLine(subS);
-
-                    k += 1;
-                    l += 1;
-
-                    if (subS.Equals(word))
-                    {
-                        result.Add(k);
-                    }
+                    result.Add(i);
                 }
             }
             
