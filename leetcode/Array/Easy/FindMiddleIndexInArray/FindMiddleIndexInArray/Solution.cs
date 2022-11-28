178213f6eeca3500b1cac2fd04028c3d5858671d
zmmohibul <18-36427-1@student.aiub.edu>
2022-11-28 07:00:00 +0600

    LeetCode - Find Middle Index in Array O(n) close

--- leetcode/Array/Easy/FindMiddleIndexInArray/FindMiddleIndexInArray/Solution.cs
+++ leetcode/Array/Easy/FindMiddleIndexInArray/FindMiddleIndexInArray/Solution.cs
@@ -10,22 +10,62 @@ namespace FindMiddleIndexInArray
     {
         public static int FindMiddleIndex(int[] nums)
         {
-            int iSum = 0;
+            var sumIndex = new Dictionary<int, int>();
+            var sum = 0;
             for (int i = 0; i < nums.Length; i++)
             {
-                int jSum = 0;
-                for (int j = i + 1; j < nums.Length; j++)
-                {
-                    jSum += nums[j];
-                }
-
-                if (jSum == iSum)
-                {
-                    return i;
-                }
-
-                iSum += nums[i];
+                sumIndex[i] = sum;
+                sum += nums[i];
             }
+
+            foreach (var (key, val) in sumIndex)
+            {
+                Console.Write($"{val} - {key}, ");
+            }
+            Console.WriteLine();
+
+            var jSum = 0;
+            for (int j = nums.Length - 1; j > 0; j--)
+            {
+                if (sumIndex[j] == jSum)
+                {
+                    return j;
+                }
+                jSum += nums[j];
+            }
+
+
+
+            //int iSum = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    int jSum = 0;
+            //    for (int j = i + 1; j < nums.Length; j++)
+            //    {
+            //        jSum += nums[j];
+            //    }
+
+            //    if (jSum == iSum)
+            //    {
+            //        return i;
+            //    }
+
+            //    iSum += nums[i];
+            //}
+
+            //var sumIndex = new Dictionary<int, int>();
+            //var sum = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    sum += nums[i];
+            //    sumIndex[sum] = i;
+            //}
+
+            //foreach(var (key, val) in sumIndex)
+            //{
+            //    Console.Write($"{val} - {key}, ");
+            //}
+            //Console.WriteLine();
 
             return -1;
         }
