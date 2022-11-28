cfdb0e34053d55bfa330ed52ab593861165fc757
zmmohibul <18-36427-1@student.aiub.edu>
2022-11-28 19:04:22 +0600

    LeetCode - Sliding Window Maximum O(n*k)

--- leetcode/Array/Hard/SlidingWindowMaximum/SlidingWindowMaximum/Solution.cs
+++ leetcode/Array/Hard/SlidingWindowMaximum/SlidingWindowMaximum/Solution.cs
@@ -8,5 +8,29 @@ namespace SlidingWindowMaximum
 {
     internal class Solution
     {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            var result = new List<int>();
+            var max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = i;
+                if (x + k - 1 < nums.Length)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        if (nums[x] > max)
+                        {
+                            max = nums[x];
+                        }
+                        x += 1;
+                    }
+                    result.Add(max);
+                }
+
+            }
+
+            return result.ToArray();
+        }
     }
 }
