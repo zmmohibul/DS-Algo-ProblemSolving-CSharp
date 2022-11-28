c1d1f2bcd692171d8f92f1e1ad5266d45206c5b1
zmmohibul <18-36427-1@student.aiub.edu>
2022-11-28 06:43:28 +0600

    LeetCode - Find Middle Index in Array

---
+++ leetcode/Array/Easy/FindMiddleIndexInArray/FindMiddleIndexInArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FindMiddleIndexInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr1 = new int[] { 2, 3, -1, 8, 4 };
+            var arr2 = new int[] { 1, -1, 4 };
+            var arr3 = new int[] { 2, 5 };
+            Console.WriteLine(Solution.FindMiddleIndex(arr1));
+            Console.WriteLine(Solution.FindMiddleIndex(arr2));
+            Console.WriteLine(Solution.FindMiddleIndex(arr3));
+        }
+    }
+}
---
+++ leetcode/Array/Easy/FindMiddleIndexInArray/FindMiddleIndexInArray/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMiddleIndexInArray
+{
+    internal class Solution
+    {
+        public static int FindMiddleIndex(int[] nums)
+        {
+            int iSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int jSum = 0;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    jSum += nums[j];
+                }
+
+                if (jSum == iSum)
+                {
+                    return i;
+                }
+
+                iSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
