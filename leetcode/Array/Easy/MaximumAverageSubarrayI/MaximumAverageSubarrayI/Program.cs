149c06d9db78876d2fec2c09c61858070bd6c624
zmmohibul <18-36427-1@student.aiub.edu>
2022-11-29 08:05:19 +0600

    LeetCode - Maximum Average Subarray I

---
+++ leetcode/Array/Easy/MaximumAverageSubarrayI/MaximumAverageSubarrayI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MaximumAverageSubarrayI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 12, -5, -6, 50, 3 };
+            Console.WriteLine(Solution.FindMaxAverage(arr, 4));
+        }
+    }
+}
---
+++ leetcode/Array/Easy/MaximumAverageSubarrayI/MaximumAverageSubarrayI/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumAverageSubarrayI
+{
+    internal class Solution
+    {
+        public static double FindMaxAverage(int[] nums, int k)
+        {
+            double sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                sum += nums[i];
+            }
+
+            double maxAverage = sum / k;
+
+            int left = 0;
+            for (int right = k; right < nums.Length; right++)
+            {
+                sum -= nums[left];
+                sum += nums[right];
+                double average = sum / k;
+                if (average > maxAverage)
+                {
+                    maxAverage = average;
+                }
+                left += 1;
+            }
+
+            return maxAverage;
+        }
+    }
+}
