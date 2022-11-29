d6a7857b8948c087a854819cec3afb8a1972d714
zmmohibul <18-36427-1@student.aiub.edu>
2022-11-29 11:23:51 +0600

    LeetCode - Find K Beauty of Number

---
+++ leetcode/Array/Easy/FindKBeautyOfNumber/FindKBeautyOfNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindKBeautyOfNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ leetcode/Array/Easy/FindKBeautyOfNumber/FindKBeautyOfNumber/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindKBeautyOfNumber
+{
+    public class Solution
+    {
+        public int DivisorSubstrings(int num, int k)
+        {
+            int rNum = num;
+            int result = 0;
+            var numList = new List<int>();
+            while (num > 0)
+            {
+                numList.Add(num % 10);
+                num /= 10;
+            }
+
+            int x = numList.Count - 1;
+            int subNum = 0;
+            int mod = 1;
+            for (int i = 0; i < k; i++)
+            {
+                subNum *= 10;
+                subNum += numList[x];
+                x -= 1;
+                mod *= 10;
+            }
+            mod /= 10;
+            if (subNum > 0 && rNum % subNum == 0)
+            {
+                result += 1;
+            }
+
+            // Console.WriteLine(mod);
+            // Console.Write(subNum);
+            for (int i = numList.Count - 1 - k; i >= 0; i--)
+            {
+                subNum = subNum % mod;
+                subNum *= 10;
+                subNum += numList[i];
+
+                if (subNum > 0 && rNum % subNum == 0)
+                {
+                    // Console.Write($" {subNum}");
+
+                    result += 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
