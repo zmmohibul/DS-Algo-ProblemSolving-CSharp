2a353e8eec84a69f3d0fc3e11690bff59c9df0a3
zmmohibul <18-36427-1@student.aiub.edu>
2022-11-29 11:47:58 +0600

    LeetCode - Find K Beauty of Number Optimized

--- leetcode/Array/Easy/FindKBeautyOfNumber/FindKBeautyOfNumber/Solution.cs
+++ leetcode/Array/Easy/FindKBeautyOfNumber/FindKBeautyOfNumber/Solution.cs
@@ -11,44 +11,55 @@ namespace FindKBeautyOfNumber
         public int DivisorSubstrings(int num, int k)
         {
             int rNum = num;
+            int gNum = num;
             int result = 0;
-            var numList = new List<int>();
-            while (num > 0)
-            {
-                numList.Add(num % 10);
-                num /= 10;
-            }
 
-            int x = numList.Count - 1;
             int subNum = 0;
             int mod = 1;
+            int rem = 0;
             for (int i = 0; i < k; i++)
             {
-                subNum *= 10;
-                subNum += numList[x];
-                x -= 1;
+                int last = rNum % 10;
+                last *= mod;
+                last += subNum;
+                subNum = last;
+
+
                 mod *= 10;
+                rNum /= 10;
             }
-            mod /= 10;
-            if (subNum > 0 && rNum % subNum == 0)
+            Console.Write(subNum);
+            int prevLast = subNum;
+            if (subNum > 0 && num % subNum == 0)
             {
                 result += 1;
             }
 
-            // Console.WriteLine(mod);
-            // Console.Write(subNum);
-            for (int i = numList.Count - 1 - k; i >= 0; i--)
-            {
-                subNum = subNum % mod;
-                subNum *= 10;
-                subNum += numList[i];
+            mod /= 10;
+            subNum /= 10;
 
-                if (subNum > 0 && rNum % subNum == 0)
+
+
+
+            while (rNum > 0)
+            {
+                int last = rNum % 10;
+                last *= mod;
+                last += (prevLast / 10);
+                prevLast = last;
+
+                Console.Write($" {last}");
+
+
+                if (last > 0 && gNum % last == 0)
                 {
-                    // Console.Write($" {subNum}");
+                    // Console.Write($" {last}");
 
                     result += 1;
                 }
+
+                // mod *= 10;
+                rNum /= 10;
             }
 
             return result;
