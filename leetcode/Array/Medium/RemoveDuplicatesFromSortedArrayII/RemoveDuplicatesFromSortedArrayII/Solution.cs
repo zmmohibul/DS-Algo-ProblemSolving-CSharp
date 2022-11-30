b23d1e5d4235fe9ca80217620f9f2651ecfd6409
zmmohibul <18-36427-1@student.aiub.edu>
2022-11-30 07:14:27 +0600

    LeetCode - Remove Duplicates from Sorted Array II

---
+++ leetcode/Array/Medium/RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayII/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicatesFromSortedArrayII
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var freq = new Dictionary<int, int>();
+            freq[nums[0]] = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                freq[nums[i]] = freq.GetValueOrDefault(nums[i], 0) + 1;
+            }
+
+            int j = 0;
+            int k = 0;
+            for (int a = 0; a < freq.Count; a++)
+            {
+                if (freq[nums[k]] >= 2)
+                {
+                    int kn = nums[k];
+                    int kc = freq[nums[k]];
+                    k += kc;
+                    nums[j] = kn;
+                    nums[j + 1] = kn;
+                    j += 2;
+
+                }
+                else
+                {
+                    nums[j] = nums[k];
+                    j += 1;
+                    k += 1;
+                }
+            }
+
+            return j;
+        }
+    }
+}
